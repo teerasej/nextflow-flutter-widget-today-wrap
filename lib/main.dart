3a5dc96dc5dada5111e65978df1b124a501f90db
Teerasej <teerasej@gmail.com>
2020-06-01 16:49:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Flutter Widget Today',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter 3 นาที: Wrap'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Wrap(
+          alignment: W`rapAlignment.start,
+          spacing: 10,
+          runSpacing: 10,
+          children: <Widget>[
+            Image.asset('images/teach1.jpg', width: 180),
+            Image.asset('images/botprogramming.jpg', width: 180),
+            Image.asset('images/seminar.jpg', width: 180),
+            Image.asset('images/inhouse1.jpg', width: 180), 
+            Image.asset('images/inhouse2.jpg', width: 180),
+          ],
+        ),
+      )
+    );
+  }
+}
